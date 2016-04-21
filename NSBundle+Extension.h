7de08dd74204f6b0432eec9b99144ecb463237b8
sunbohong <sunbohong@gmail.com>
2016-04-21 15:18:41 +0800

    添加获取应用名的类方法

---
+++ NSBundle+Extension.h
@@ -0,0 +1,20 @@
+//
+//  NSBundle+Extension.h
+//  sunbohong
+//
+//  Created by sunbohong on 16/4/21.
+//  Copyright © 2016年 sunbohong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSBundle (Extension)
+
+/**
+ *  获取应用名
+ *
+ *  @return 应用名
+ */
++ (NSString *)appName;
+
+@end
---
+++ NSBundle+Extension.m
@@ -0,0 +1,28 @@
+//
+//  NSBundle+Extension.m
+//  sunbohong
+//
+//  Created by sunbohong on 16/4/21.
+//  Copyright © 2016年 sunbohong. All rights reserved.
+//
+
+#import "NSBundle+Extension.h"
+
+@implementation NSBundle (Extension)
+
+/**
+ *  优先获取CFBundleDisplayName，如果获取为nil，则尝试获取CFBundleName
+ *
+ *  @return 应用名
+ */
++ (NSString *)appName {
+    NSBundle *bundle = [NSBundle mainBundle];
+    
+    NSString *appName = [bundle objectForInfoDictionaryKey:@"CFBundleDisplayName"];
+    if (!appName) {
+        appName = [bundle objectForInfoDictionaryKey:@"CFBundleName"];
+    }
+    return appName;
+}
+
+@end
